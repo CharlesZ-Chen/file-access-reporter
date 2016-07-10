e49ad4f4fa8e140fc0725a9f10ab7b8f391886cb
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-07-10 16:46:46 -0400

    init commit of new PathValue

--- src/analysis/value/PathValue.java
+++ src/analysis/value/PathValue.java
@@ -1,5 +1,35 @@
 package analysis.value;
 
-public class PathValue {
+import org.checkerframework.dataflow.analysis.AbstractValue;
+import org.checkerframework.dataflow.cfg.node.Node;
+
+public class PathValue extends TreeValue<Node, StrValue, PathValue> implements AbstractValue<PathValue> {
+
+    public PathValue(TreeValue.Type type, Object leafValue) {
+        super(type, leafValue);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public PathValue leastUpperBound(PathValue other) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void solveVar(Node target, Node expression) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public PathValue copy() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void reduce() {
+        // TODO Auto-generated method stub
+    }
 
 }
