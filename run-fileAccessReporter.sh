12d703d6e7602de6e9f0aa3c336c84a78a6eb8ed
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-07-11 17:10:22 -0400

    add processor and shell script as cmd entry

---
+++ run-fileAccessReporter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WORKING_DIR=$(pwd)
+ROOT=$(cd $(dirname $0)/.. && pwd)
+
+DATAFLOW="$ROOT"/dataflow-framework/dataflow
+JAVACUTIL="$ROOT"/dataflow-framework/javacutil
+JSR308_LANGTOOL="$ROOT"/jsr308-langtools
+
+DATAFLOW_JAR="$DATAFLOW"/dist/dataflow.jar
+JAVACUTIL_JAR="$JAVACUTIL"/dist/javacutil.jar
+JAVAC_JAR="$JSR308_LANGTOOL"/dist/lib/javac.jar
+
+FILE_ACCESS_REPORTER="$ROOT"/file-access-reporter
+BIN="$FILE_ACCESS_REPORTER"/bin
+
+JARS="$DATAFLOW_JAR":"$JAVACUTIL_JAR":"$JAVAC_JAR"
+
+OUTPUT_DIR="$WORKING_DIR"/analysis-report
+if [ ! -d "$OUTPUT_DIR" ] ; then
+    mkdir "$OUTPUT_DIR"
+fi
+
+javac -cp "$BIN":"$JARS" -processor playground.FileAccessProcessor "$@"
---
+++ src/playground/FileAccessProcessor.java
@@ -0,0 +1,134 @@
+package playground;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.tools.Diagnostic.Kind;
+
+import org.checkerframework.dataflow.analysis.BackwardAnalysis;
+import org.checkerframework.dataflow.analysis.BackwardAnalysisImpl;
+import org.checkerframework.dataflow.cfg.CFGBuilder;
+import org.checkerframework.dataflow.cfg.CFGVisualizer;
+import org.checkerframework.dataflow.cfg.ControlFlowGraph;
+import org.checkerframework.dataflow.cfg.DOTCFGVisualizer;
+import org.checkerframework.dataflow.cfg.UnderlyingAST;
+import org.checkerframework.dataflow.cfg.UnderlyingAST.CFGMethod;
+import org.checkerframework.dataflow.cfg.node.Node;
+import org.checkerframework.javacutil.BasicTypeProcessor;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.util.TreePathScanner;
+
+import analysis.FileAccessStore;
+import analysis.FileAccessTransfer;
+import analysis.value.PathValue;
+
+@SupportedAnnotationTypes("*")
+public class FileAccessProcessor extends BasicTypeProcessor {
+
+    protected CFGVisualizer<PathValue, FileAccessStore, FileAccessTransfer> cfgVisualizer;
+    protected CompilationUnitTree rootTree;
+    protected ClassTree currentClassTree;
+    protected MethodTree currentMethodTree;
+    protected StringBuilder finalReport;
+
+    public FileAccessProcessor() {
+       super();
+       Map<String, Object> args = new HashMap<>();
+       args.put("outdir", "./analysis-report");
+       args.put("checkerName", "");
+       cfgVisualizer = new DOTCFGVisualizer<>();
+       cfgVisualizer.init(args);
+    }
+
+    @Override
+    public void typeProcessingOver() {
+        cfgVisualizer.shutdown();
+        if (this.finalReport == null) {
+            initFinalReport();
+            preFinalReport();
+            finalReport.append("no file access found!");
+        }
+        System.out.println(finalReport.toString());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+      return SourceVersion.latestSupported();
+    }
+
+    @Override
+    protected TreePathScanner<?, ?> createTreePathScanner(CompilationUnitTree root) {
+        rootTree = root;
+        return new TreePathScanner<Void, Void>() {
+            @Override
+            public Void visitClass(ClassTree node, Void p) {
+                currentClassTree = node;
+                return super.visitClass(node, p);
+            }
+
+            @Override
+            public Void visitMethod(MethodTree node, Void p) {
+                currentMethodTree = node;
+                ControlFlowGraph cfg = CFGBuilder.build(rootTree, processingEnv, currentMethodTree, currentClassTree);
+                analysisCFG(cfg);
+                return super.visitMethod(node, p);
+            }
+        };
+    }
+
+    protected void analysisCFG(ControlFlowGraph cfg) {
+        BackwardAnalysis<PathValue, FileAccessStore, FileAccessTransfer> analysis = new BackwardAnalysisImpl<>(new FileAccessTransfer());
+        UnderlyingAST ast = (CFGMethod) cfg.getUnderlyingAST();
+        String cfgIdentifier;
+        if (ast.getKind() == UnderlyingAST.Kind.METHOD) {
+            CFGMethod cfgm = (CFGMethod) ast;
+            cfgIdentifier = cfgm.getClass().getName().toString() + "#" + cfgm.getMethod().getName().toString();
+        } else {
+            cfgIdentifier = "non-method";
+        }
+
+        analysis.performAnalysis(cfg);
+
+        processingEnv.getMessager().printMessage(Kind.NOTE, "anlysis cfg: " + cfgIdentifier);
+
+        buildReport(cfgIdentifier, ((FileAccessStore) analysis.getEntrySotre()).getFilePathMap());
+
+        cfgVisualizer.visualize(cfg, cfg.getEntryBlock(), analysis);
+    }
+
+    protected void buildReport(String cfgIdentifier, Map<Node, PathValue> filePathMap) {
+        if (filePathMap.isEmpty()) {
+            return;
+        }
+
+        if (this.finalReport == null) {
+            initFinalReport();
+            preFinalReport();
+        }
+
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.append("Analysis Result of " + cfgIdentifier + ":\n");
+        for (Entry<Node, PathValue> entry : filePathMap.entrySet()) {
+            entry.getValue().reduce(); // final reduce before out put
+            sBuilder.append("\t" + entry.getKey() + " = " + entry.getValue() + "\n");
+        }
+        sBuilder.append("\n");
+        this.finalReport.append(sBuilder);
+    }
+
+    protected void initFinalReport() {
+        this.finalReport = new StringBuilder();
+    }
+
+    protected void preFinalReport() {
+        finalReport.append("====================================\n");
+        finalReport.append("    R E P O R T                     \n");
+        finalReport.append("====================================\n");
+    }
+}
